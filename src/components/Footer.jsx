b6f5a5e81c13ea712910fd1ff63aee113453e23b
bruce <bruce.monnerville@hotmail.fr>
2024-10-07 16:20:57 +0200

    responsive presentation + footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "../styles/Footer.scss"
+
+function Footer() {
+
+    return (
+        <footer></footer>
+    )
+
+}
+
+export default Footer
--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import Home from './pages/Home.jsx'
 import Header from './components/Header.jsx'
 import Travaux from './pages/Travaux.jsx'
 import Fiche from './pages/Fiche.jsx'
+import Footer from './components/Footer.jsx'
 
- 
+
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -13,12 +14,13 @@ const root = createRoot(container);
 root.render(
     <React.StrictMode>
         <Router>
-          <Header />
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/travaux" element={<Travaux />} />
                 <Route path='fiche/:id' element={<Fiche />} />
             </Routes>
-                  </Router>
+            <Footer />
+        </Router>
     </React.StrictMode>
 )
